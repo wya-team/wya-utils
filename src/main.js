aba6f953eaac47767e126063110b7d8afa411217
chenchen <729041629@qq.com>
2018-05-24 15:28:24 +0800

    字数个数
    添加正真意义上的字数个数在计算而不是字符个数

--- src/main.js
+++ src/main.js
@@ -484,3 +484,26 @@ export const accDiv = (arg1, arg2, opts = {}) => {
 export const createMixins = (...list) => target => {
 	Object.assign(target.prototype, ...list);
 };
+
+/**
+ * 用word方式计算正文字数
+ * @param str 
+ */
+export const getCpmisWords = (str) => {
+    let sLen = 0;
+    try{
+        //先将回车换行符做特殊处理
+        str = str.replace(/(\r\n+|\s+|　+)/g,"龘");
+        //处理英文字符数字，连续字母、数字、英文符号视为一个单词
+        str = str.replace(/[\x00-\xff]/g,"m");
+        //合并字符m，连续字母、数字、英文符号视为一个单词
+        str = str.replace(/m+/g,"*");
+        //去掉回车换行符
+        str = str.replace(/龘+/g,"");
+        //返回字数
+        sLen = str.length;
+    }catch(e){
+
+    }
+    return sLen;
+}
