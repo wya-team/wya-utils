858cf152e0f1c87129512d5e52f4d71adee5ede1
Mr.Z <zrd0921@qq.com>
2019-04-09 13:46:54 +0800

    update: validator

--- src/regex.js
+++ src/regex.js
@@ -25,34 +25,57 @@ class Manager {
 	}
 	/**
 	 * for async-validator
+	 * rule: {
+	 * 	name, // 用于提示必填
+	 * 	type,
+	 * 	required,
+	 * 	message,
+	 * 	msg,
+	 * 	, // 依赖其他选项时
+	 * }
 	 */
-	validator(rule, value, callback, opts = {}) {
-		let errorMsg;
-		if (typeof value === 'string') {
-			value = value.trim();
-		}
-		if (rule.required && !value) {
-			errorMsg = `${rule.name || ''}必填`;
-			callback(errorMsg);
-			return false;
-		}
-		let rules = rule.type instanceof Array ? rule.type : [rule.type];
-
-		for (let i = 0; i < rules.length; i++) {
-			let type = rules[i];
-			let val = value;
-			if (type == 'mobile') {
-				val = val || '';
-				val = val.replace(/\s/g, '');
+	async validator(rule, value, callback, opts = {}) {
+		try {
+			let errorMsg;
+			if (typeof value === 'string') {
+				value = value.trim();
 			}
+			// TODO: 看业务需要是否处理成Promise
+			let required = typeof rule.required === 'function'
+				? await rule.required()
+				: rule.required;
 
-			if (this[type] && val && !this[type].test(val)) {
-				errorMsg = rule.message || rule.msg || this[`${type}Msg`];
-				rules.length - 1 == i && callback(errorMsg);
-			} else {
-				callback();
-				break;
+			if ( required && !value ) {
+				errorMsg = `${rule.name || ''}必填`;
+				callback(errorMsg);
+				return false;
 			}
+			let rules = rule.type instanceof Array ? rule.type : [rule.type];
+
+			for (let i = 0; i < rules.length; i++) {
+				let type = rules[i];
+				let val = value;
+				if (type == 'mobile') {
+					val = val || '';
+					val = val.replace(/\s/g, '');
+				}
+
+				// TODO: 看业务需要是否处理成Promise
+				let isError = typeof type === 'function' 
+					? await type()
+					: this[type] && val && !this[type].test(val);
+
+				if (type) {
+					errorMsg = rule.message || rule.msg || this[`${type}Msg`];
+					rules.length - 1 == i && callback(errorMsg);
+				} else {
+					callback();
+					break;
+				}
+			}
+		} catch (e) {
+			callback(`验证有误`);
+			console.error(`[@wya/utils - regex]: 验证有误`, e);
 		}
 	}
 	_generate(rules) {
