43e21587579eae460b4f46b1e24aad9f2ec667f7
Mr.Z <zrd0921@qq.com>
2019-05-14 14:54:14 +0800

    update:test

--- src/__test__/cache.test.js
+++ src/__test__/cache.test.js
@@ -3,39 +3,22 @@ import { Storage, Cookie } from '../cache';
 describe('cache.js', () => {
 	test('验证api', () => {
 		expect(Storage === window.Storage).toBe(false);
-		expect(typeof Storage.get).toBe('function');
-		expect(typeof Storage.set).toBe('function');
-		expect(typeof Storage.remove).toBe('function');
+		[Storage, Cookie].forEach(target => {
+			expect(typeof target.get).toBe('function');
+			expect(typeof target.set).toBe('function');
+			expect(typeof target.remove).toBe('function');
 
-		expect(typeof Cookie.get).toBe('function');
-		expect(typeof Cookie.set).toBe('function');
-		expect(typeof Cookie.remove).toBe('function');
+			target.set('user', { name: 'wya' });
+			expect(target.get('user').name).toBe('wya');
 
+			target.set('user', '{"name": "wya1"}');
+			expect(target.get('user').name).toBe('wya1');
 
-		Storage.set('user', { name: 'wya' });
-		expect(Storage.get('user').name).toBe('wya');
+			target.set('user', 'wya');
+			expect(target.get('user')).toBe('wya');
 
-		Storage.set('user', '{"name": "wya1"}');
-		expect(Storage.get('user').name).toBe('wya1');
-
-		Storage.set('user', 'wya');
-		expect(Storage.get('user')).toBe('wya');
-
-		Storage.remove('user');
-		expect(Storage.get('user')).toBe(null);
-
-
-		Cookie.set('user', { name: 'wya' });
-		expect(Cookie.get('user').name).toBe('wya');
-
-		Cookie.set('user', '{"name": "wya1"}');
-		expect(Cookie.get('user').name).toBe('wya1');
-
-		Cookie.set('user', 'wya');
-		expect(Cookie.get('user')).toBe('wya');
-
-		Cookie.remove('user');
-		expect(Cookie.get('user')).toBe(null);
-
+			target.remove('user');
+			expect(target.get('user')).toBe(null);
+		});
 	});
 });
