6b7b3c0b2871a03af02ee0fd407aea405c0c624b
Mr.Z <zrd0921@qq.com>
2020-11-19 18:52:13 +0800

    [build] v1.6.3

--- scripts/commit-lint.js
+++ scripts/commit-lint.js
@@ -1,12 +1,13 @@
 const fs = require('fs-extra');
 
-const commitRE = /^(revert: )?(fix|feat|opti|docs|perf|test|types|build|chore|refactor|breaking change)(\(.+\))?: .{1,50}/;
+const commitRE = /^(revert: )?(fix|feat|docs|perf|test|types|chore|refactor|breaking change)(\(.+\))?: .{1,50}/;
 const mergeRE = /Merge branch /;
+const buildRE = /\[build\] /;
 
 const gitParams = process.env.HUSKY_GIT_PARAMS;
 const commitMsg = fs.readFileSync(gitParams, 'utf-8').trim();
 
-if (!commitRE.test(commitMsg) && !mergeRE.test(commitMsg)) {
+if (!commitRE.test(commitMsg) && !mergeRE.test(commitMsg) && !buildRE.test(commitMsg)) {
 	console.error(
 		`invalid commit message: "${commitMsg}".
 
@@ -24,10 +25,8 @@ if (!commitRE.test(commitMsg) && !mergeRE.test(commitMsg)) {
 		- test：增加测试
 		- chore：构建过程或辅助工具的变动
 		- refactor：重构（即不是新增功能，也不是修改bug的代码变动）
-		- opti: 优化
 		- perf
 		- types
-		- build
 		- breaking change
 		- Merge branch 'foo' into 'bar'
 		`
